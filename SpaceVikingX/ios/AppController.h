d8fd675fbbccf132a5a5c0b4853f16ea99ee572b
An Jun <anjsoft@gmail.com>
2013-06-15 08:44:52 +0800

    Initial Commit

---
+++ SpaceVikingX/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  SpaceVikingXAppController.h
+//  SpaceVikingX
+//
+//  Created by An Jun on 13-6-15.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ SpaceVikingX/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  SpaceVikingXAppController.h
+//  SpaceVikingX
+//
+//  Created by An Jun on 13-6-15.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
